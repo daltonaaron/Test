a4a76b141c82777838f8d7d3dba07ed7474255df
Aaron Dalton <daltonaaron@hotmail.com>
2025-04-24 21:01:56 -0700

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Mia Dai on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
