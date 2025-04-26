deb803d6c389d327f46e4015d0f7d538f90027a7
Aaron Dalton <daltonaaron@hotmail.com>
2025-04-26 14:04:49 -0700

    Addind basis for step counter.  No more App Ids available

--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -5,6 +5,53 @@
 //  Created by Mia Dai on 4/24/25.
 //
 //  adding stuff here
+
+import Foundation
+import HealthKit
+import Observation
+
+enum HealthError:  Error {
+    case healthDataNotAvialable
+}
+
+
+@Observable
+
+class HealthStore {
+    
+    
+    var healthStore: HKHealthStore?
+    var lastError: Error?
+    
+    
+    init() {
+    
+    
+        if HKHealthStore.isHealthDataAvailable() {
+        healthStore = HKHealthStore()
+    } else {
+        lastError = HealthError.healthDataNotAvialable
+    }
+}
+
+    func requestAuthorization() async {
+        
+        guard let stepType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount) else { return }
+        guard let healthStore = self.healthStore else {return}
+        
+        do {
+            
+            try await healthStore.requestAuthorization(toShare: [], read: [stepType])
+            
+        } catch {
+            lastError = error
+        }
+    }
+    }
+
+
+
+
 import SwiftUI
 
 struct ContentView: View {
